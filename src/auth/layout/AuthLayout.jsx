05e612aefca1f369d7ce6e3745ddd27647864165
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-22 17:26:11 -0300

    erase background on login

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -10,7 +10,7 @@ export const AuthLayout = ({ children, title = '' }) => {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: '#aed581', padding: 4 }}
+      sx={{ minHeight: '100vh', padding: 4 }}
     >
 
       <Grid item
